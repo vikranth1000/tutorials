540aa4234b6f15ffc62d5255362609e5c1061dbe
Alvino Angelo <153690899+aangelo9@users.noreply.github.com>
2025-07-10 12:53:12 -0400

    TutorTask589_Refactor_prophet_tutorial_into_class (#590)

---
+++ tutorial_prophet/src/prophet_model.py
@@ -0,0 +1,84 @@
+"""
+Import as:
+
+import tutorial_prophet.src.prophet_model as tpsrprmo
+"""
+
+import logging
+from typing import Dict, Optional
+
+import pandas as pd
+import prophet
+import sklearn
+
+_LOG = logging.getLogger(__name__)
+
+
+# #############################################################################
+# ProphetForecastModel
+# #############################################################################
+
+
+class ProphetForecastModel:
+    """
+    Facebook Prophet wrapper.
+    """
+
+    def __init__(
+        self, config: Dict, *, holidays: Optional[pd.DataFrame] = None
+    ) -> None:
+        """
+        Initialize the Prophet model.
+
+        :param config: Prophet hyperparameters
+        :param holidays: data of holidays
+        """
+        self.config = config
+        self.holidays = holidays
+        self.model = prophet.Prophet(**config, holidays=holidays)
+        self.fitted = False
+
+    def fit(self, df: pd.DataFrame) -> None:
+        """
+        Fit the Prophet model on the given DataFrame.
+
+        :param df: training data
+        """
+        for col in df.columns:
+            if col not in ["ds", "y"]:
+                _LOG.info("Adding regressor: %s", col)
+                self.model.add_regressor(col)
+        self.model.fit(df)
+        self.fitted = True
+
+    def predict(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Predict future values using the fitted model.
+
+        :param df: data of future timestamps
+        :return: forecasted data
+        """
+        if not self.fitted:
+            raise RuntimeError("Model must be fitted before prediction.")
+        return self.model.predict(df)
+
+    def evaluate(self, df: pd.DataFrame) -> Dict[str, float]:
+        """
+        Evaluate forecast against actuals.
+
+        :param df: forecasted and actual data
+        :return: forecast accuracy metrics
+        """
+        y_true = df["y"]
+        y_pred = df["yhat"]
+        return {
+            "mae": sklearn.metrics.mean_absolute_error(y_true, y_pred),
+            "rmse": sklearn.metrics.root_mean_squared_error(y_true, y_pred),
+            "mape": (abs(y_true - y_pred) / y_true).mean() * 100,
+        }
+
+    def get_model(self) -> prophet.Prophet:
+        """
+        Get the internal Prophet object.
+        """
+        return self.model
--- tutorial_prophet/src/tutorial_prophet.py
+++ tutorial_prophet/src/tutorial_prophet.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.16.6
+#       jupytext_version: 1.17.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -14,6 +14,22 @@
 # ---
 
 # %% [markdown]
+# CONTENTS:
+# - [Description](#description)
+# - [Imports](#imports)
+# - [Config](#config)
+# - [Generate data](#generate-data)
+# - [Fit the model](#fit-the-model)
+# - [Predict](#predict)
+# - [Analyze the results](#analyze-the-results)
+#     - [`Prophet.plot()`](#`prophet.plot()`)
+#     - [`Prophet.plot_components()`](#`prophet.plot_components()`)
+#     - [Prophet.plot_plotly()](#prophet.plot_plotly())
+# - [Performance metrics](#performance-metrics)
+# - [Cross Validation](#cross-validation)
+
+# %% [markdown]
+# <a name='description'></a>
 # # Description
 
 # %% [markdown]
@@ -24,6 +40,7 @@
 # - Official docs: `https://facebook.github.io/prophet/docs/quick_start.html`
 
 # %% [markdown]
+# <a name='imports'></a>
 # # Imports
 
 # %%
@@ -34,21 +51,20 @@
 # %%
 import logging
 
+import helpers.hdbg as hdbg
+import helpers.hpandas as hpandas
+import helpers.hprint as hprint
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import prophet as prh
-import prophet.diagnostics as diagnostics
 import plotly.graph_objects as go
+import prophet.diagnostics as diagnostics
 import prophet.plot as plo
 import prophet.utilities as prhu
-
-import sklearn.metrics as metrics
 import scipy.stats as stats
+import sklearn.metrics as metrics
 
-import helpers.hprint as hprint
-import helpers.hdbg as hdbg
-import helpers.hpandas as hpanda
+from tutorial_prophet.src.prophet_model import ProphetForecastModel
 
 # %%
 hdbg.init_logger(verbosity=logging.INFO)
@@ -58,6 +74,7 @@ _LOG = logging.getLogger(__name__)
 hprint.config_notebook()
 
 # %% [markdown]
+# <a name='config'></a>
 # # Config
 
 # %%
@@ -75,7 +92,13 @@ config = {
         "weekly_amp_sin": 5.0,
         "weekly_amp_cos": 2.5,
         # List of holidays and their impact.
-        "holidays_dates": ['2020-12-25', '2021-12-25', '2022-12-25', "2023-12-25", "2024-12-25"],
+        "holidays_dates": [
+            "2020-12-25",
+            "2021-12-25",
+            "2022-12-25",
+            "2023-12-25",
+            "2024-12-25",
+        ],
         "holidays_impact": 2.5,
         # Autoregression params.
         "ar_order": 1,
@@ -86,104 +109,129 @@ config = {
     },
     "model": {
         # Defines trend shape.
-        "growth":"linear", 
-        "yearly_seasonality":False, 
+        "growth": "linear",
+        "yearly_seasonality": False,
         # Integer defines Fourier order.
-        "weekly_seasonality":1, 
-        "daily_seasonality":False, 
-        "n_changepoints":0, 
+        "weekly_seasonality": 1,
+        "daily_seasonality": False,
+        "n_changepoints": 0,
         # Prophet internally scales the target variable.
-        "scaling":"minmax", 
+        "scaling": "minmax",
         # Confidence iterval width.
-        "interval_width":0.95,
+        "interval_width": 0.95,
         # Number of MCMC samples; If `mcmc_samples = 0`, the model produces just a point estimate  of each
         # parameter instead of the full distribution.
-        "mcmc_samples":300,
+        "mcmc_samples": 300,
         # Set lower prior scale to narrow the confidence interval.
-        "holidays_prior_scale":0.1,
+        "holidays_prior_scale": 0.1,
     },
 }
 print(config)
 
 # %% [markdown]
+# <a name='generate-data'></a>
 # # Generate data
 
 # %%
 # Generate date range.
-dates = pd.date_range(start=config["train_start_date"], end=config["test_end_date"], freq='D')
+dates = pd.date_range(
+    start=config["train_start_date"], end=config["test_end_date"], freq="D"
+)
 time = np.arange(len(dates))
 # Define linear trend.
 y_trend = config["data"]["slope"] * time + config["data"]["intercept"]
 # Define the seasonality factor.
 p_weekly = 7
-y_weekly_seasonality = config["data"]["weekly_amp_sin"] * np.sin(2 * np.pi * time / p_weekly) + config["data"]["weekly_amp_cos"] * np.cos(2 * np.pi * time / p_weekly)
+y_weekly_seasonality = config["data"]["weekly_amp_sin"] * np.sin(
+    2 * np.pi * time / p_weekly
+) + config["data"]["weekly_amp_cos"] * np.cos(2 * np.pi * time / p_weekly)
 # Define holidays impact.
 holiday_effect = np.zeros(len(dates))
-holiday_effect[np.isin(dates.date, pd.to_datetime(config["data"]["holidays_dates"]).date)] = config["data"]["holidays_impact"]
+holiday_effect[
+    np.isin(dates.date, pd.to_datetime(config["data"]["holidays_dates"]).date)
+] = config["data"]["holidays_impact"]
 # Define white noise.
 np.random.seed(config["data"]["seed"])
-noise = np.random.normal(loc=0, scale=config["data"]["noise_sigma"], size=len(time))
+noise = np.random.normal(
+    loc=0, scale=config["data"]["noise_sigma"], size=len(time)
+)
 # Add autoregressive behavior.
 y = np.zeros(len(time))
 y[0] = y_trend[0] + y_weekly_seasonality[0] + holiday_effect[0] + noise[0]
 for i in range(1, len(time)):
-    y[i] = config["data"]["phi"] * y[i-1] + y_trend[i] + y_weekly_seasonality[i] + holiday_effect[i] + noise[i]
+    y[i] = (
+        config["data"]["phi"] * y[i - 1]
+        + y_trend[i]
+        + y_weekly_seasonality[i]
+        + holiday_effect[i]
+        + noise[i]
+    )
 # Columns "ds" for timestamp and "y" for target variable is necessary in prophet. https://facebook.github.io/prophet/docs/quick_start.html
 df = pd.DataFrame({"ds": dates, "y": y})
 # Add lagged value of the target as a feature.
 df["y.lag1"] = df["y"].shift(1)
 # The first row for the lagged feature is NaN; we have to remove it because the model cannot handle NaNs
 df = df.dropna()
-_LOG.info(hpanda.df_to_str(df, log_level=logging.INFO))
+_LOG.info(hpandas.df_to_str(df, log_level=logging.INFO))
 
 # %%
-df.set_index("ds")["y"].plot(title="Original data", ylabel="Target variable", xlabel="Time")
+df.set_index("ds")["y"].plot(
+    title="Original data", ylabel="Target variable", xlabel="Time"
+)
 
 # %% [markdown]
+# <a name='fit-the-model'></a>
 # # Fit the model
 
 # %%
 start_date_filter = df["ds"] >= config["train_start_date"]
 end_date_filter = df["ds"] <= config["train_end_date"]
 df_train = df[start_date_filter & end_date_filter]
-_LOG.info(hpanda.df_to_str(df_train, log_level=logging.INFO))
+_LOG.info(hpandas.df_to_str(df_train, log_level=logging.INFO))
 
 # %% [markdown]
 # In Prophet, holidays can be customized to account for their effects on time series data.
-# Each holiday is defined as a specific date or set of recurring dates. 
-# Additionally, you can extend the influence of a holiday to a range of days using 
+# Each holiday is defined as a specific date or set of recurring dates.
+# Additionally, you can extend the influence of a holiday to a range of days using
 # the `lower_window` and `upper_window` parameters:
 #
 #  - `lower_window`: Specifies how many days before the holiday the effect starts.
 #  - `upper_window`: Specifies how many days after the holiday the effect ends.
 #
 # For example:
-#  - To include Christmas Eve as part of the Christmas holiday effect, set 
+#  - To include Christmas Eve as part of the Christmas holiday effect, set
 #    `lower_window=-1` and `upper_window=0`.
-#  - To include Black Friday in addition to Thanksgiving, set 
+#  - To include Black Friday in addition to Thanksgiving, set
 #    `lower_window=0` and `upper_window=1`.
 #
-# Additionally, you can customize the strength of the holiday effect for each holiday 
-# using the `prior_scale` parameter. This allows finer control over the regularization 
+# Additionally, you can customize the strength of the holiday effect for each holiday
+# using the `prior_scale` parameter. This allows finer control over the regularization
 # of each holiday's effect, balancing flexibility and overfitting.
 #
 #
 
 # %%
 # Construct a Dataframe with holidays.
-holidays_df = pd.DataFrame({
-    'holiday': ['Christmas 2020', 'Christmas 2021', 'Christmas 2022', "Christmas 2023", "Christmas 2024"],
-    'ds': pd.to_datetime(config["data"]["holidays_dates"]),
-    'lower_window': 0, 
-    'upper_window': 0,   
-})
-_LOG.info(hpanda.df_to_str(holidays_df, log_level=logging.INFO))
+holidays_df = pd.DataFrame(
+    {
+        "holiday": [
+            "Christmas 2020",
+            "Christmas 2021",
+            "Christmas 2022",
+            "Christmas 2023",
+            "Christmas 2024",
+        ],
+        "ds": pd.to_datetime(config["data"]["holidays_dates"]),
+        "lower_window": 0,
+        "upper_window": 0,
+    }
+)
+_LOG.info(hpandas.df_to_str(holidays_df, log_level=logging.INFO))
 
 # %%
-model = prh.Prophet(**config["model"], holidays=holidays_df)
-# Explicitly add the lagged feature as an external regressor.
-model.add_regressor('y.lag1')
-model.fit(df_train)
+forecaster = ProphetForecastModel(config["model"], holidays_df)
+forecaster.fit(df_train)
+model = forecaster.get_model()
 
 # %% [markdown]
 # Dimensionality:
@@ -192,7 +240,7 @@ model.fit(df_train)
 # - delta (MxN array): The slope change at each of N changepoints
 # - beta (MxK matrix): Coefficients for K seasonality features
 # - sigma_obs (Mx1 array): Noise level
-# - M=1 (meaning that only the point estimate is available) if MAP estimation, i.e. `mcmc_samples=0` 
+# - M=1 (meaning that only the point estimate is available) if MAP estimation, i.e. `mcmc_samples=0`
 #
 # The number M (posterior samples) depends on:
 # - `mcmc_samples` provided by the user
@@ -202,7 +250,7 @@ model.fit(df_train)
 # %% [markdown]
 # Scaling:
 # - Prophet perofrorms scaling internally, i.e. time is mapped to be on [0, 1] and Y is scaled by model.y_scale
-# - Where `y_scale` is controlled by the user and can be `absmax` or `minmax` scaling 
+# - Where `y_scale` is controlled by the user and can be `absmax` or `minmax` scaling
 # - TODO(Grisha): consider rescaling coefficients in order to compare them to the ground truth
 
 # %%
@@ -227,8 +275,10 @@ _LOG.info("Slope point estimate = %s", estimated_slope.mean())
 # Use `regressor_coefficients()` to extract external regressors' coefficient. Unlike the other coeficients these
 # already takes into account the internal scaling, thus can be directly compared to the original coefficients.
 regressor_coefficients = prhu.regressor_coefficients(model)
-_LOG.info(hpanda.df_to_str(regressor_coefficients, log_level=logging.INFO))
-_LOG.info("True value of the autoregression coefficient = %s", config["data"]["phi"])
+_LOG.info(hpandas.df_to_str(regressor_coefficients, log_level=logging.INFO))
+_LOG.info(
+    "True value of the autoregression coefficient = %s", config["data"]["phi"]
+)
 
 # %% [markdown]
 # Description:
@@ -246,31 +296,33 @@ _LOG.info("True value of the autoregression coefficient = %s", config["data"]["p
 
 # %%
 # Use this table to identify coefficients' indices in the regressors' matrix.
-col_names = model.make_all_seasonality_features(df_train)[0].columns 
-coefficients_df = pd.DataFrame(model.params["beta"], columns = col_names)
-_LOG.info(hpanda.df_to_str(coefficients_df, log_level=logging.INFO))
+col_names = model.make_all_seasonality_features(df_train)[0].columns
+coefficients_df = pd.DataFrame(model.params["beta"], columns=col_names)
+_LOG.info(hpandas.df_to_str(coefficients_df, log_level=logging.INFO))
 
 # %% [markdown]
+# <a name='predict'></a>
 # # Predict
 
 # %%
 start_date_filter = df["ds"] >= config["test_start_date"]
 end_date_filter = df["ds"] <= config["test_end_date"]
 df_test = df[start_date_filter & end_date_filter].reset_index(drop=True)
-_LOG.info(hpanda.df_to_str(df_test, log_level=logging.INFO))
+_LOG.info(hpandas.df_to_str(df_test, log_level=logging.INFO))
 
 # %%
 # Predict on the entire data and then analyze the tran and test split seperately to gain more insights.
-forecast = model.predict(df)
+forecast = forecaster.predict(df)
 forecast = forecast.merge(df, how="inner", on=["ds"])
 forecast["residual"] = forecast["y"] - forecast["yhat"]
-_LOG.info(hpanda.df_to_str(forecast, log_level=logging.INFO))
+_LOG.info(hpandas.df_to_str(forecast, log_level=logging.INFO))
 
 # %% [markdown]
+# <a name='analyze-the-results'></a>
 # # Analyze the results
 
 # %%
-ins_forecast = forecast[forecast["ds"]<=config["train_end_date"]]
+ins_forecast = forecast[forecast["ds"] <= config["train_end_date"]]
 ins_forecast.tail(5)
 
 # %%
@@ -285,8 +337,16 @@ plt.show()
 # Residuals are normal distributed, hence the modle is working fine.
 
 # %%
-plt.plot(ins_forecast["ds"], ins_forecast["yhat"], label="Point Estimate", ls='--', c='#0072B2')
-plt.plot(ins_forecast["ds"], ins_forecast["y"], label="Observed data", color="blue")
+plt.plot(
+    ins_forecast["ds"],
+    ins_forecast["yhat"],
+    label="Point Estimate",
+    ls="--",
+    c="#0072B2",
+)
+plt.plot(
+    ins_forecast["ds"], ins_forecast["y"], label="Observed data", color="blue"
+)
 
 # Plot confidence/prediction intervals
 plt.fill_between(
@@ -300,6 +360,7 @@ plt.fill_between(
 plt.title("Observed vs predicted data, train set")
 
 # %% [markdown]
+# <a name='`prophet.plot()`'></a>
 # ### `Prophet.plot()`
 #
 # The `Prophet.plot()` function is used to visualize the forecast generated by the Prophet model. It provides a comprehensive view of the predicted trend and uncertainty intervals over the forecast horizon.
@@ -316,6 +377,7 @@ plt.title("Observed vs predicted data, train set")
 model.plot(ins_forecast, include_legend=True)
 
 # %% [markdown]
+# <a name='`prophet.plot_components()`'></a>
 # ### `Prophet.plot_components()`
 #
 # The `Prophet.plot_components()` function provides a breakdown of the individual components contributing to the forecast. This helps understand **why** the forecast behaves as it does.
@@ -335,7 +397,7 @@ model.plot(ins_forecast, include_legend=True)
 fig2 = model.plot_components(ins_forecast)
 
 # %%
-oos_forecast = forecast[forecast["ds"]>=config["test_start_date"]]
+oos_forecast = forecast[forecast["ds"] >= config["test_start_date"]]
 oos_forecast.tail(5)
 
 # %%
@@ -345,6 +407,7 @@ model.plot(oos_forecast)
 model.plot_components(oos_forecast)
 
 # %% [markdown]
+# <a name='prophet.plot_plotly()'></a>
 # ### Prophet.plot_plotly()
 # The Prophet.plot_plotly() function provides an interactive visualization of the forecast using the Plotly library. This is an alternative to Prophet.plot(), which uses Matplotlib for static plots.
 #
@@ -357,11 +420,11 @@ fig = plo.plot_plotly(model, forecast)
 # Add ground truth (actual values)
 fig.add_trace(
     go.Scatter(
-        x=df_test['ds'],
-        y=df_test['y'],
-        mode='markers',
-        name='Ground Truth',
-        marker=dict(color='red', size=5)
+        x=df_test["ds"],
+        y=df_test["y"],
+        mode="markers",
+        name="Ground Truth",
+        marker=dict(color="red", size=5),
     )
 )
 
@@ -378,19 +441,23 @@ fig.show()
 
 
 # %% [markdown]
+# <a name='performance-metrics'></a>
 # # Performance metrics
 #
 # Prophet provides performance metric calculation only on the cross validation data. To test the performance of prophet model on test split we will be using sklearn metrics.
 
 # %%
 # Calculate metrics
-mae = metrics.mean_absolute_error(oos_forecast['y'], oos_forecast['yhat'])
-rmse = metrics.mean_squared_error(oos_forecast['y'], oos_forecast['yhat'])
-mape = (abs(oos_forecast['y'] - oos_forecast['yhat']) / oos_forecast['y']).mean() * 100
+mae = metrics.mean_absolute_error(oos_forecast["y"], oos_forecast["yhat"])
+rmse = metrics.mean_squared_error(oos_forecast["y"], oos_forecast["yhat"])
+mape = (
+    abs(oos_forecast["y"] - oos_forecast["yhat"]) / oos_forecast["y"]
+).mean() * 100
 
 _LOG.info("MAE=%s, RMSE=%s, MAPE=%s", mae, rmse, mape)
 
 # %% [markdown]
+# <a name='cross-validation'></a>
 # # Cross Validation
 #
 # `cross_validation` method does not take in the dataset as input so the cross validation is done on the data used in `model.fit()`
@@ -417,22 +484,22 @@ _LOG.info("MAE=%s, RMSE=%s, MAPE=%s", mae, rmse, mape)
 df_cv = diagnostics.cross_validation(
     model=model,
     # Use 2 years as initial training period.
-    initial='730 days',
+    initial="730 days",
     # Move rolling window every 180 days.
-    period='180 days',     
-    horizon='365 days'
+    period="180 days",
+    horizon="365 days",
 )
 
 
 # %%
 df_perf = diagnostics.performance_metrics(df_cv)
-_LOG.info(hpanda.df_to_str(df_perf, log_level=logging.INFO))
+_LOG.info(hpandas.df_to_str(df_perf, log_level=logging.INFO))
 
 # %%
 df_cv.head()
 
 # %%
 # Plot Mean Absolute Percentage Error (MAPE)
-fig = plo.plot_cross_validation_metric(df_cv, metric='mape')
+fig = plo.plot_cross_validation_metric(df_cv, metric="mape")
 
 # %%
